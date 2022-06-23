6a6d9c09331a4d6b3df36a21a532f5f276715b2c
MajeedKazemi <majeed@dgp.toronto.edu>
2022-06-23 18:03:49 -0400

    added join + added sensing_askandwait block generators

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * 
+ *
  * Copyright 2019 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,64 +15,87 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { ContinuousFlyout, ContinuousMetrics, ContinuousToolbox } from "@blockly/continuous-toolbox";
+import * as Blockly from "blockly";
+
+import { controls } from "./controls.js";
+import { event } from "./event.js";
+import { inputoutput } from "./inputoutput.js";
+import { logic } from "./logic.js";
+import { loops } from "./loops.js";
+import { math } from "./math.js";
+import { toolbox } from "./toolbox.js";
+import { variables } from "./variables.js";
 
 /**
- * @fileoverview Example of including Blockly with using Webpack with 
+ * @fileoverview Example of including Blockly with using Webpack with
  *               defaults: (English lang & JavaScript generator).
  * @author samelh@google.com (Sam El-Husseini)
  */
 
-import * as Blockly from 'blockly';
-import {toolbox} from './toolbox.js';
-import {loops} from './loops.js';
-import {logic} from './logic.js';
-import {controls} from './controls.js';
-import {variables} from './variables.js';
-import {inputoutput} from './inputoutput.js';
-import {math} from './math.js';
-import {event} from './event.js';
-import {ContinuousToolbox, ContinuousFlyout, ContinuousMetrics} from '@blockly/continuous-toolbox';
-
-
-Blockly.Msg.MATH_MODULO_TITLE = "%1 mod %2"
-Blockly.Msg.TEXT_PRINT_TITLE = "say %1"
+Blockly.Msg.MATH_MODULO_TITLE = "%1 mod %2";
+Blockly.Msg.TEXT_PRINT_TITLE = "say %1";
 
 document.addEventListener("DOMContentLoaded", function () {
-      
-    const workspace = Blockly.inject('blocklyDiv',
-        {
-            plugins: {
-              'toolbox': ContinuousToolbox,
-              'flyoutsVerticalToolbox': ContinuousFlyout,
-              'metricsManager': ContinuousMetrics,
-            },
-            toolbox: toolbox,
-            media: 'media/',
-            renderer: 'zelos'
-        });
-    
-    workspace.createVariable("my variable")
+  const workspace = Blockly.inject("blockly-container", {
+    plugins: {
+      toolbox: ContinuousToolbox,
+      flyoutsVerticalToolbox: ContinuousFlyout,
+      metricsManager: ContinuousMetrics,
+    },
+    toolbox: toolbox,
+    media: "media/",
+    renderer: "zelos",
+  });
 
-    const lang = 'JavaScript';
-    const button = document.getElementById('blocklyButton');
-    button.addEventListener('click', function () {
-        //alert("Check the console for the generated output.");
-        const code = Blockly[lang].workspaceToCode(workspace);
-        const divCode = document.getElementById('code');
-        const divConsole = document.getElementById('console');
-        console.log(code);
-        divCode.textContent = code;
-        const F = new Function(code);
-        F();
-    })
+  workspace.createVariable("my variable");
+
+  const lang = "JavaScript";
+  const button = document.getElementById("run-button");
+  button.addEventListener("click", function () {
+    //alert("Check the console for the generated output.");
+    const code = Blockly[lang].workspaceToCode(workspace);
+    const divCode = document.getElementById("code");
+    const divConsole = document.getElementById("console");
+    console.log(code);
+    divCode.textContent = code;
+
+    eval("(async () => {" + code + "})()");
+  });
 });
 
+Blockly.defineBlocksWithJsonArray(loops);
+Blockly.defineBlocksWithJsonArray(logic);
+Blockly.defineBlocksWithJsonArray(controls);
+Blockly.defineBlocksWithJsonArray(variables);
+Blockly.defineBlocksWithJsonArray(inputoutput);
+Blockly.defineBlocksWithJsonArray(math);
+Blockly.defineBlocksWithJsonArray(event);
 
+/**
+ * the getValueFromUserInput() function is called within the `sensing_askandwait` block
+ * it will return the value from the user-input asyncronously
+ */
+async function getValueFromUserInput() {
+  let waitForPressResolve;
 
-Blockly.defineBlocksWithJsonArray(loops)
-Blockly.defineBlocksWithJsonArray(logic)
-Blockly.defineBlocksWithJsonArray(controls)
-Blockly.defineBlocksWithJsonArray(variables)
-Blockly.defineBlocksWithJsonArray(inputoutput)
-Blockly.defineBlocksWithJsonArray(math)
-Blockly.defineBlocksWithJsonArray(event)
+  function waitForPress() {
+    return new Promise((resolve) => (waitForPressResolve = resolve));
+  }
+
+  function btnResolver() {
+    if (waitForPressResolve) waitForPressResolve();
+  }
+
+  let btn = document.getElementById("user-input-form");
+  btn.addEventListener("submit", (e) => {
+    btnResolver();
+    e.preventDefault();
+  });
+
+  await waitForPress();
+
+  const inputEl = document.getElementById("user-input");
+
+  return inputEl.value;
+}
--- src/inputoutput.js
+++ src/inputoutput.js
@@ -1,104 +1,160 @@
-import * as Blockly from 'blockly';
-Blockly.Msg.SENSING_ASKANDWAIT = "ask %1 and wait"
-Blockly.Msg.SENSING_ANSWER = "answer"
+import * as Blockly from "blockly";
+
+Blockly.Msg.SENSING_ASKANDWAIT = "ask %1 and wait";
+Blockly.Msg.SENSING_ANSWER = "answer";
 
 export var inputoutput = [
-    {
-      "type": "sensing_askandwait",
-      "message0": Blockly.Msg.SENSING_ASKANDWAIT,
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "TEXT"
-        }
-      ],
-      "previousStatement": null,
-      "nextStatement": null,
-      'style': 'variable_blocks',
-    },
+  {
+    type: "sensing_askandwait",
+    message0: Blockly.Msg.SENSING_ASKANDWAIT,
+    args0: [
+      {
+        type: "input_value",
+        name: "TEXT",
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    style: "variable_blocks",
+  },
 
-    {
-      "type": "sensing_answer",
-      "message0": Blockly.Msg.SENSING_ANSWER,
-      "checkboxInFlyout": true,
-      
-      "output": "String",
-      "outputShape": Blockly.OUTPUT_SHAPE_ROUND,
-      'style': 'variable_blocks',
-    },
-    {
-      "type": "text_join",
-      'message0': 'join %1 %2',
-      'args0': [
-        {
-          'type': 'input_value',
-          'name': 'A',
-          'check': 'String',
-        },
-        {
-          'type': 'input_value',
-          'name': 'B',
-          'check': 'String',
-        },
-      ],
-      'output': 'String',
-      'inputsInline': true,
-      'style': 'logic_blocks',
-    },
-    {
-      'type': 'text_charAt',
-      'message0': 'letter %1 of %2',
-      'args0': [
-        {
-          'type': 'input_value',
-          'name': 'A',
-          'check': 'Number',
-        },
-        {
-          'type': 'input_value',
-          'name': 'B',
-          'check': 'String',
-        },
-  
-      ],
-      'output': 'String',
-      'style': 'logic_blocks',
-      'helpUrl': '%{BKY_TEXT_CHARAT_HELPURL}',
-      'inputsInline': true,
-      
-    },
-    
-]
+  {
+    type: "sensing_answer",
+    message0: Blockly.Msg.SENSING_ANSWER,
+    checkboxInFlyout: true,
 
-Blockly.JavaScript['sensing_askandwait'] = function(block) {
+    output: "String",
+    outputShape: Blockly.OUTPUT_SHAPE_ROUND,
+    style: "variable_blocks",
+  },
+  {
+    type: "text_join",
+    message0: "join %1 %2",
+    args0: [
+      {
+        type: "input_value",
+        name: "A",
+        check: "String",
+      },
+      {
+        type: "input_value",
+        name: "B",
+        check: "String",
+      },
+    ],
+    output: "String",
+    inputsInline: true,
+    style: "logic_blocks",
+  },
+  {
+    type: "text_charAt",
+    message0: "letter %1 of %2",
+    args0: [
+      {
+        type: "input_value",
+        name: "A",
+        check: "Number",
+      },
+      {
+        type: "input_value",
+        name: "B",
+        check: "String",
+      },
+    ],
+    output: "String",
+    style: "logic_blocks",
+    helpUrl: "%{BKY_TEXT_CHARAT_HELPURL}",
+    inputsInline: true,
+  },
+];
+
+Blockly.JavaScript["sensing_askandwait"] = function (block) {
   // Prompt function.
-  if (block.getField('TEXT')) {
+  if (block.getField("TEXT")) {
     // Internal message.
-    var msg = Blockly.JavaScript.quote_(block.getFieldValue('TEXT'));
+    var msg = Blockly.JavaScript.quote_(block.getFieldValue("TEXT"));
   } else {
     // External message.
-    var msg = Blockly.JavaScript.valueToCode(block, 'TEXT',
-        Blockly.JavaScript.ORDER_NONE) || '\'\'';
+    var msg =
+      Blockly.JavaScript.valueToCode(
+        block,
+        "TEXT",
+        Blockly.JavaScript.ORDER_NONE
+      ) || "''";
   }
-  var code = 'var hidden_var = window.prompt(' + msg + ');\n'
-  return code
-}
+  var code = "var hidden_var = window.prompt(" + msg + ");\n";
+  return code;
+};
 
-Blockly.JavaScript['sensing_answer'] = function(block) {
-  var code = 'hidden_var'
+Blockly.JavaScript["sensing_answer"] = function (block) {
+  var code = "hidden_var";
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
-}
+};
 
-Blockly.JavaScript['text_charAt'] = function(block) {
+Blockly.JavaScript["text_charAt"] = function (block) {
   //asdf
-}
+};
 
-Blockly.JavaScript['text_print'] = function(block) {
-  const msg = Blockly.JavaScript.valueToCode(block, 'TEXT',
-      Blockly.JavaScript.ORDER_NONE) || "''";
-  return `
+Blockly.JavaScript["text_print"] = function (block) {
+  const msg =
+    Blockly.JavaScript.valueToCode(
+      block,
+      "TEXT",
+      Blockly.JavaScript.ORDER_NONE
+    ) || "''";
+  return (
+    `
   var divConsole = document.getElementById("console");
-  var content = document.createTextNode(`+msg+`);
+  var content = document.createTextNode(` +
+    msg +
+    `);
   divConsole.appendChild(content);
-  divConsole.innerHTML += '<br>';`;
+  divConsole.innerHTML += '<br>';`
+  );
+};
+
+Blockly.JavaScript["sensing_askandwait"] = function (block) {
+  const msg =
+    Blockly.JavaScript.valueToCode(
+      block,
+      "TEXT",
+      Blockly.JavaScript.ORDER_NONE
+    ) || "''";
+
+  const sec =
+    Blockly.JavaScript.valueToCode(
+      block,
+      "second",
+      Blockly.JavaScript.ORDER_NONE
+    ) || "''";
+
+  return (
+    `
+    var divConsole = document.getElementById("console");
+    var content = document.createTextNode(` +
+    msg +
+    `);
+    divConsole.appendChild(content);
+    divConsole.innerHTML += '<br>';
+    
+    var hidden_var = await getValueFromUserInput();
+  `
+  );
+};
+
+Blockly.JavaScript["text_join"] = function (block) {
+  const A =
+    Blockly.JavaScript.valueToCode(block, "A", Blockly.JavaScript.ORDER_NONE) ||
+    "''";
+
+  const B =
+    Blockly.JavaScript.valueToCode(block, "B", Blockly.JavaScript.ORDER_NONE) ||
+    "''";
+
+  return [
+    `
+    (${A}.toString() + ${B}.toString())
+  `,
+    Blockly.JavaScript.ORDER_FUNCTION_CALL,
+  ];
 };
